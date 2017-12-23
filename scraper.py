9b0ec90256439c34a5f7ec1ed710119bc72564e4
Jane Lusby <jlusby42@gmail.com>
2017-12-22 21:18:57 -0800

    more local changes

--- scraper.py
+++ scraper.py
@@ -56,7 +56,7 @@ def scrape_area(area):
         filters=settings.FILTERS)
 
     results = []
-    gen = cl_h.get_results(sort_by='newest', geotagged=True, limit=3)
+    gen = cl_h.get_results(sort_by='newest', geotagged=True, limit=20)
     while True:
         try:
             result = next(gen)
